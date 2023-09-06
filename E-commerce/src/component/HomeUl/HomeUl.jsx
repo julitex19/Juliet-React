0a146226291e35abd3ead99e9d3febd3336d1f38
Juliet <okwygod201@gmail.com>
2023-09-06 16:03:41 +0100

    made changes

--- E-commerce/src/component/HomeUl/HomeUl.jsx
+++ E-commerce/src/component/HomeUl/HomeUl.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import styles from "./HomeUl.module.css";
+import { FaAngleRight } from'react-icons/fa'
 
 function HomeUl() {
   return (
 
     <>
     <div>
+
       <ul className={styles.list}>
+        <li className={styles.lists}> 
         <li>Women's Fashion </li>
+        <FaAngleRight/>
+        </li>
+
+        <li className={styles.lists}>
         <li>men's Fashion</li>
+        <FaAngleRight/>
+        </li>
         <li>Electronics</li>
         <li>Home & Lifestyle</li>
         <li>Medicine</li>
